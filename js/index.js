eb4793ae078e29bb57ca2e0550960412078b6806
kooijr002 <31730809+kooijr002@users.noreply.github.com>
2017-10-21 16:14:11 +0200

    cdhsvj

--- js/index.js
+++
@@ -1,7 +0,0 @@
-var plusje = document.querySelector('.boek');
-var eentje = document.querySelector('[href="#"]');
-
-function show() {
-    plusje.classList.toggle('laatzien');
-}
-eentje.addEventListener('click', show);
