affc8f566998cdc9a0b4d43c52ce31912426ba44
kooijr002 <31730809+kooijr002@users.noreply.github.com>
2017-10-23 14:33:33 +0200

    extra verhalen hovers

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+//var toev = document.querySelector('.add');
+var eentje = document.querySelector('#add');
 
-var eentje = document.querySelector('.add');
 
-function show() {
+function show('eentje') {
+    //toev.classList.toggle('toevoegd');
     eentje.classList.toggle('toevoegen');
 }
+//toev.addEventListener('click', show);
 eentje.addEventlistener('click', show);
