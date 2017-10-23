5959e17e2c7e10930398246f64ed1ea06128856e
kooijr002 <31730809+kooijr002@users.noreply.github.com>
2017-10-23 20:17:39 +0200

    proberen 2de micro interactie

--- js/index.js
+++ js/index.js
@@ -1,31 +1,18 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
-//var toev = document.querySelector('.add');
 
 
-var plus = document.querySelector("footer img:nth-of-type(2)");
+var plus = document.querySelector("footer img:nth-of-type(3)");
 var een = document.querySelector("#eentje");
 
-plus.addEventListener("click", function() {
- een.classList.toggle("yay");
+
+plus.addEventListener("click", function () {
+    een.classList.toggle("yay");
 });
 
 
-/*
-var hartje = document.querySelector('footer img:nth-of-type(1)');
-var element = document.querySelector('footer img:nth-of-type(1)');
+var hart = document.querySelector("footer img:nth-of-type(1)");
+var tweede = document.querySelector("#hartje");
 
-function change() {
-    hartje.classlist.toggle('.heart');
-}
-var plusje = document.querySelector('.add');
-plus.addEventlistener('click', show);
- plus.classList.toggle('footer img:nth-of-type(2)');function show() {
-
-    een.classList.toggle('');
-}
-
-een.addEventListener('click', show);
-}
-
-hartje.addEventListener('click', change);
-*/
+hart.addEventListener("click", function () {
+    tweede.classlist.toggle("rood");
+});
