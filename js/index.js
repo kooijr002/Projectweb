4a144873986b193cfcb8f64a668db62f5eff99da
kooijr002 <31730809+kooijr002@users.noreply.github.com>
2017-10-23 15:58:34 +0200

    GOEDE JAVASCRIPT GEDAAN

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 //var toev = document.querySelector('.add');
-var eentje = document.querySelector('#add');
 
 
-function show('eentje') {
-    //toev.classList.toggle('toevoegd');
-    eentje.classList.toggle('toevoegen');
+var plus = document.querySelector("footer img:nth-of-type(2)");
+var een = document.querySelector("#eentje");
+
+plus.addEventListener("click", function() {
+ een.classList.toggle("yay");
+});
+
+
+/*
+var hartje = document.querySelector('footer img:nth-of-type(1)');
+var element = document.querySelector('footer img:nth-of-type(1)');
+
+function change() {
+    hartje.classlist.toggle('.heart');
 }
-//toev.addEventListener('click', show);
-eentje.addEventlistener('click', show);
+var plusje = document.querySelector('.add');
+plus.addEventlistener('click', show);
+ plus.classList.toggle('footer img:nth-of-type(2)');function show() {
+
+    een.classList.toggle('');
+}
+
+een.addEventListener('click', show);
+}
+
+hartje.addEventListener('click', change);
+*/
