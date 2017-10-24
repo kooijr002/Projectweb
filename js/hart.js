dffe25959b0fafb9f2c9618f30da8de8bcae09e2
kooijr002 <31730809+kooijr002@users.noreply.github.com>
2017-10-24 15:14:01 +0200

    hard life

--- js/hart.js
+++ js/hart.js
@@ -1,10 +1,9 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*bron=https://css-tricks.com/video-screencasts/150-hey-designers-know-one-thing-javascript-recommend/*/
 
+var hart = document.querySelector("footer img:nth-of-type(1)");
+var tweede = document.querySelector("footer img:nth-of-type(2)");
 
-var plus = document.querySelector("footer img:nth-of-type(3)");
-var een = document.querySelector("#eentje");
-
-plus.addEventListener("click", function () {
-    een.classList.toggle("yay");
+hart.addEventListener("click", function () {
+    tweede.classlist.toggle("rood");
 });
