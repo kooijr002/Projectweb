d2facba079bb567f6b5db2aed13837d0cfe606a8
kooijr002 <31730809+kooijr002@users.noreply.github.com>
2017-10-25 14:20:14 +0200

    oops

--- js/hart.js
+++ js/hart.js
@@ -1,9 +1,9 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*bron=https://css-tricks.com/video-screencasts/150-hey-designers-know-one-thing-javascript-recommend/*/
 
-var hart = document.querySelector('#hartje');
-var tweede = document.querySelector('#roodje');
+var hart = document.querySelector("footer img:nth-of-type(1)");
+var tweede = document.querySelector('#red');
 
 hart.addEventListener("click", function () {
-    tweede.classlist.toggle('#roodje');
+    tweede.classlist.toggle('.rood');
 });
